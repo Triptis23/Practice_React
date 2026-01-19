b3e9d30f8f1807b055a1bab40e463facd57024a1
Tripti <triptisingh2305@gmail.com>
2026-01-19 18:39:19 +0530

    Revert "Remove localstorage folder"
    This reverts commit 8337d4c3cf353e556fc4960178d40d2ff07e570f.

---
+++ localstorage/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import './App.css'
+import Buttonupper from './Components/buttonupper';
+import Timer from './Components/timer';
+
+function App() {
+  
+  return (
+    <>
+     <Buttonupper text={"type ur text"}/>
+     {/* <Timer/> */}
+    </>
+  );
+}
+
+export default App
